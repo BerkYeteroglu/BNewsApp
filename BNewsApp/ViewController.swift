b9b91996fdff9272354d2b1476195f48a75d18eb
berkyeterogluth@gmail.com <berkyeterogluth@gmail.com>
2023-10-04 22:58:29 +0300

    Initial Commit

---
+++ BNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BNewsApp
+//
+//  Created by Berk Yeteroğlu on 4.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
